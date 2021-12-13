eb10b94b562130b237489cc50673cafd1cbe9fc6
yl301114 <yl301114@gmail.com>
2021-12-13 21:37:05 +0800

    更新

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,44 @@
 
+let xMousePos = 0;
+let yMousePos = 0;
+let lastScrolledLeft = 0;
+let lastScrolledTop = 0;
+let cursor_1_x,cursor_1_y,cursor_2_x,cursor_2_y;
 
-
-
-const cursor = document.querySelector(".cursor");
-const cursor_2 = document.querySelector(".cursor-2");
-
-
-document.addEventListener("mousemove", (e) => {
-  cursor.setAttribute(
-    "style",
-    "top: " + (e.pageY - 18) + "px; left: " + (e.pageX - 18) + "px;"
-  );
-  cursor_2.setAttribute(
-    "style",
-    "top: " + (e.pageY - 4) + "px; left: " + (e.pageX - 4) + "px;"
-  );
+$(document).mousemove(function(event) {
+  captureMousePosition(event);
 });
-document.addEventListener("scroll", (e) => {
-    cursor.setAttribute(
-      "style",
-      "top: " + (e.pageY - 18) + "px; left: " + (e.pageX - 18) + "px;"
-    );
-    cursor_2.setAttribute(
-        "style",
-        "top: " + (e.pageY - 4) + "px; left: " + (e.pageX - 4) + "px;"
-      );
-});
+
+$(window).scroll(function(event){
+  if(lastScrolledLeft != $(document).scrollLeft()){
+    xMousePos -= lastScrolledLeft;
+    lastScrolledLeft = $(document).scrollLeft();
+    xMousePos += lastScrolledLeft;
+    cursor_1_x = xMousePos-18;
+    cursor_2_x = xMousePos-4;
+  }
+  if(lastScrolledTop != $(document).scrollTop()){
+    yMousePos -= lastScrolledTop;
+    lastScrolledTop = $(document).scrollTop();
+    yMousePos += lastScrolledTop;
+    cursor_1_y = yMousePos-18;
+    cursor_2_y = yMousePos-4;
+  }
+  $(".cursor").css("top",cursor_1_y).css("left",cursor_1_x);
+  $(".cursor-2").css("top",cursor_2_y).css("left",cursor_2_x);
+})
+
+function captureMousePosition(event){
+  xMousePos = event.pageX;
+  yMousePos = event.pageY;
+  cursor_1_x = xMousePos-18;
+  cursor_2_x = xMousePos-4;
+  cursor_1_y = yMousePos-18;
+  cursor_2_y = yMousePos-4;
+  $(".cursor").css("top",cursor_1_y).css("left",cursor_1_x);
+  $(".cursor-2").css("top",cursor_2_y).css("left",cursor_2_x);
+}
+
 
 $(".next").click(function () {
     $(this).parents(".color-box").addClass("active");
